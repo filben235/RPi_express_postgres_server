807b8d580990bc88b69eed6d30dc6c2f10e2f56e
Filip Bengs <filip.bengs@netikka.fi>
2021-08-06 13:16:07 +0300

    updated POST and GET endpoints to use SQL database

--- controllers/contentControllers.js
+++ controllers/contentControllers.js
@@ -1,8 +1,39 @@
+let pool = require("../config/database");
+
+//enables inserting into database from web
 exports.postContent = function (req, res) {
   let content = req.body.content;
-  res.send(`the content posted was: ${content}`);
+
+  //checks that submitted contntent is either string or a number
+  let dataValid = typeof content == "number" || typeof content == "string";
+
+  if (dataValid) {
+    //DO NOT INSERT USER GENERATED VALUES INTO THE STRING DIRECTLY, SQL INJECTION RISK
+    let insertSQL = "INSERT INTO content (contentinfo) VALUES ($1);";
+    let value = [content];
+
+    // Pass an array of values as the second
+    // argument for pool.query() method to
+    // build the query string safely.
+    pool.query(insertSQL, value, (error, result) => {
+      if (error) {
+        res.status(400).send(error);
+      } else {
+        res.status(200).send("Saved to database.\n");
+      }
+    });
+  } else {
+    res.status(400).send("Please check that your data types are correct");
+  }
 };
 
+//Gets most recent submitted content from db and returns as JSON
 exports.getLatestContent = function (req, res) {
-  res.send("The latest submitted content was");
+  pool.query(
+    "SELECT * FROM content ORDER BY id DESC LIMIT 1;",
+    (error, results) => {
+      if (error) throw error;
+      res.status(200).json(results.rows);
+    }
+  );
 };
